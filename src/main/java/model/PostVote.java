0baacfeaa7b612bb053217f757a30e83812c96a0
Topoy <rinz.ler@aiesec.net>
2020-09-02 11:40:51 +0300

    added main model classes and connected MySQL

---
+++ src/main/java/model/PostVote.java
@@ -0,0 +1,61 @@
+package model;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "post_votes")
+public class PostVote
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    //@Column(name = "user_id")
+    @ManyToOne(cascade = CascadeType.ALL)
+    private User user;
+    //@Column(name = "post_id")
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Post post;
+    private LocalDateTime time;
+    private byte value;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
+
+    public byte getValue() {
+        return value;
+    }
+
+    public void setValue(byte value) {
+        this.value = value;
+    }
+}
---
+++ src/main/java/model/StatusType.java
@@ -0,0 +1,8 @@
+package model;
+
+public enum StatusType
+{
+    NEW,
+    ACCEPTED,
+    DECLINED
+}
