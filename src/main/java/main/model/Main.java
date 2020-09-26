bfb1803892e69a5919181e98156688431b3c516a
Topoy <rinz.ler@aiesec.net>
2020-09-26 07:50:33 +0300

    ready for pull

--- src/main/java/main/model/Main.java
+++ src/main/java/main/model/Main.java
@@ -1,5 +1,12 @@
 package main.model;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -24,7 +31,7 @@ public class Main implements CommandLineRunner
     }
     public static void main(String[] args)
     {
-     /*   StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+        /*StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
         Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
         SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
 
